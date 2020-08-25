cced1fa76d74266d9714092ec5405d306195caf3
Jesús Montero <jesusmontero.developer@gmail.com>
2020-08-24 19:00:43 -0500

    Add styled components for principal page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,56 @@
 import React from 'react';
 import './App.css';
+import image from './cryptomonedas.png';
+
+import styled from '@emotion/styled';
 
 function App() {
+
+	const Container = styled.div`
+		max-width: 900px;
+		margin: 0 auto;
+		@media (min-width: 992px) {
+			display: grid;
+			grid-template-columns: repeat(2, 1fr);
+			column-gap: 2rem;
+		}
+	`;
+
+	const Image = styled.img`
+		max-width: 100%;
+		margin-top: 5 rem;
+	`;
+
+	const Heading = styled.h1`
+		font-family: 'Bebas Neue', cursive;
+		color: #ffffff;
+		text-align: left;
+		font-weight: 700;
+		font-size: 50px;
+		margin-bottom: 50px;
+		margin-top: 80px;
+
+		&::after {
+			content: '';
+			width: 100px;
+			height: 6px;
+			background-color: #66A2FE;
+			display: block;
+		}
+	`;
+
 	return (
-		<h1>hello</h1>
+		<Container>
+			<div>
+				<Image
+					src={image}
+					alt='image crypto'
+				/>
+			</div>
+			<div>
+				<Heading>Quote cryptocurrencies instantly</Heading>
+			</div>
+		</Container>
 	);
 }
 
